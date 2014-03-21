d71ca8fa3b9e970a98a782833e30e8ad977f200c
Leongum <luyongluy@gmail.com>
2014-03-21 16:15:29 +0800

    add walk event

---
+++ RevolUtioN/CoreDate/Walk_Event.h
@@ -0,0 +1,21 @@
+//
+//  Walk_Event.h
+//  WalkFun
+//
+//  Created by leon on 14-3-21.
+//  Copyright (c) 2014年 Beyond. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Walk_Event : NSObject
+
+@property (nonatomic, retain) NSString * eType;
+@property (nonatomic, retain) NSNumber * eId;
+@property (nonatomic, retain) NSNumber * eWin;
+@property (nonatomic, retain) NSNumber * times;
+@property (nonatomic, retain) NSString * lati;
+@property (nonatomic, retain) NSString * longi;
+@property (nonatomic, retain) NSNumber * power;
+
+@end
---
+++ RevolUtioN/CoreDate/Walk_Event.m
@@ -0,0 +1,32 @@
+//
+//  Walk_Event.m
+//  WalkFun
+//
+//  Created by leon on 14-3-21.
+//  Copyright (c) 2014年 Beyond. All rights reserved.
+//
+
+#import "Walk_Event.h"
+#import "RORDBCommon.h"
+
+@implementation Walk_Event
+
+@dynamic eType;
+@dynamic eId;
+@dynamic eWin;
+@dynamic times;
+@dynamic lati;
+@dynamic longi;
+@dynamic power;
+
+-(void)initWithDictionary:(NSDictionary *)dict{
+    self.eType = [RORDBCommon getStringFromId:[dict valueForKey:@"eType"]];
+    self.eId = [RORDBCommon getNumberFromId:[dict valueForKey:@"eId"]];
+    self.eWin = [RORDBCommon getNumberFromId:[dict valueForKey:@"eWin"]];
+    self.times = [RORDBCommon getNumberFromId:[dict valueForKey:@"times"]];
+    self.lati = [RORDBCommon getStringFromId:[dict valueForKey:@"lati"]];
+    self.longi = [RORDBCommon getStringFromId:[dict valueForKey:@"longi"]];
+    self.power = [RORDBCommon getNumberFromId:[dict valueForKey:@"power"]];
+}
+
+@end
